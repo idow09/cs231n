5094218165ca16ef06c0c6604079978027148c2e
Efrayim <ephiephi@gmail.com>
2019-04-02 17:06:04 +0300

    Affine forward works fine

--- spring1718_assignment2_v2/cs231n/layers.py
+++ spring1718_assignment2_v2/cs231n/layers.py
@@ -6,29 +6,23 @@ def affine_forward(x, w, b):
     """
     Computes the forward pass for an affine (fully-connected) layer.
 
-    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    The input x has shape (n, d_1, ..., d_k) and contains a minibatch of n
     examples, where each example x[i] has shape (d_1, ..., d_k). We will
     reshape each input into a vector of dimension D = d_1 * ... * d_k, and
     then transform it to an output vector of dimension M.
 
     Inputs:
-    - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    - x: A numpy array containing input data, of shape (n, d_1, ..., d_k)
     - w: A numpy array of weights, of shape (D, M)
     - b: A numpy array of biases, of shape (M,)
 
     Returns a tuple of:
-    - out: output, of shape (N, M)
+    - out: output, of shape (n, M)
     - cache: (x, w, b)
     """
-    out = None
-    ###########################################################################
-    # TODO: Implement the affine forward pass. Store the result in out. You   #
-    # will need to reshape the input into rows.                               #
-    ###########################################################################
-    pass
-    ###########################################################################
-    #                             END OF YOUR CODE                            #
-    ###########################################################################
+    n = x.shape[0]
+    x = x.reshape(n, -1)  # n * D
+    out = np.dot(x, w) + b.reshape(1, -1)  # broadcast b as a row vector
     cache = (x, w, b)
     return out, cache
 
